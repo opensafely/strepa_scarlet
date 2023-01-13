81ba47caa99f40b811af53b228cdca7e0aafcd7b
Christine Cunningham <5705329+ccunningham101@users.noreply.github.com>
2023-01-13 09:35:38 -0800

    black and flake

--- analysis/report/plot_measures_report.py
+++ analysis/report/plot_measures_report.py
@@ -2,33 +2,33 @@ import pandas as pd
 import argparse
 from report_utils import plot_measures
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--measure_path", help="path of combined measure file")
     args = parser.parse_args()
     return args
 
+
 def main():
     args = parse_args()
-    
-    
-  
+
     df = pd.read_csv(args.measure_path, parse_dates=["date"])
     df = df.loc[df["value"] != "[REDACTED]", :]
-    
+
     # for each group, plot the measure
     for group in df["name"].unique():
         df_subset = df.loc[df["name"] == group, :]
         df_subset["value"] = df_subset["value"].astype(float)
         df_subset["rate"] = df_subset["value"] * 1000
-    
+
         df_subset["group"] = df_subset["group"].astype(str)
-        if len(df_subset["group"].unique())==1:
+        if len(df_subset["group"].unique()) == 1:
             # no breakdown
-            
+
             plot_measures(
-            df_subset,
-                filename=f"report/plot_measures",
+                df_subset,
+                filename="report/plot_measures",
                 column_to_plot="rate",
                 y_label="Rate per 1000",
                 as_bar=False,
@@ -36,15 +36,14 @@ def main():
             )
         else:
             plot_measures(
-            df_subset,
-            filename=f"report/plot_measures_{group}",
-            column_to_plot="rate",
-            y_label="Rate per 1000",
-            as_bar=False,
-            category="group",
-        )
+                df_subset,
+                filename=f"report/plot_measures_{group}",
+                column_to_plot="rate",
+                y_label="Rate per 1000",
+                as_bar=False,
+                category="group",
+            )
 
-    
-    
+
 if __name__ == "__main__":
     main()
